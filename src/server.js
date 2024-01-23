25c66e11579fa2cc9f4a92e4be721f8ca8bcf711
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-01-23 23:33:10 +0300

    updates

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
+import { StatusCodes } from 'http-status-codes';
 
 const app = express();
 const port = 3000;
 
 // localhost:3000/
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.status(StatusCodes.CREATED);
+    res.send('hello, You!');
 });
 
 app.listen(port, () => {
